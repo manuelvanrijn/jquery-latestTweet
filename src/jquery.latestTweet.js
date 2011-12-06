e56ba9b8c2018fca1d818272808fbcbb70a232f0
Manuel van Rijn <vanrijn@auxilium.nl>
2011-12-06 16:48:25 +0100

    tabs and spaces

--- src/jquery.latestTweet.js
+++ src/jquery.latestTweet.js
@@ -6,51 +6,50 @@
 */
 
 ;(function($, window, document, undefined) {
-    var pluginName = 'latestTweet';
-    var defaults = {
-        username: 'manuelvanrijn'
-    };
+  var pluginName = 'latestTweet';
+  var defaults = {
+    username: 'manuelvanrijn'
+  };
 
-    // The actual plugin constructor
-    function Plugin(element, options) {
-        this.element = element;
-        this.options = $.extend({}, defaults, options);
+  function Plugin(element, options) {
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
 
-        this._defaults = defaults;
-        this._name = pluginName;
+    this._defaults = defaults;
+    this._name = pluginName;
 
-        this.init();
+    this.init();
+  }
+
+  Plugin.prototype = {
+    init: function() {
+      var url = this.build_url();
+      var that = this;
+      $.getJSON(url, function(data) {
+        var formattedText = that.formatTweet(data[0].text);
+        $(that.element).html(formattedText);
+      });
+    },
+    build_url: function() {
+      var proto = ('https:' == document.location.protocol ? 'https:' : 'http:');
+      return proto+'//api.twitter.com/1/statuses/user_timeline.json?screen_name='+this.options.username+'&count=1&callback=?';
+    },
+    formatTweet: function(text) {
+      var regexpLinkUrl = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+      var regexpLinkUser = /[\@]+([A-Za-z0-9-_]+)/gi;
+      var regexpLinkHash = / [\#]+([A-Za-z0-9-_]+)/gi;
+
+      text = text.replace(regexpLinkUrl, "<a href=\"$1\">$1</a>");
+      text = text.replace(regexpLinkUser, "<a href=\"http://twitter.com/$1\">@$1</a>");
+      return text.replace(regexpLinkHash, ' <a href="http://search.twitter.com/search?q=&tag=$1&lang=all">#$1</a>');
     }
+  };
 
-    Plugin.prototype = {
-        init: function() {
-            var url = this.build_url();
-            var that = this;
-            $.getJSON(url, function(data) {
-				var formattedText = that.formatTweet(data[0].text);
-                $(that.element).html(formattedText);
-            });
-        },
-        build_url: function() {
-            var proto = ('https:' == document.location.protocol ? 'https:' : 'http:');
-            return proto+'//api.twitter.com/1/statuses/user_timeline.json?screen_name='+this.options.username+'&count=1&callback=?';
-        },
-		formatTweet: function(text) {
-			var regexpLinkUrl = /((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
-			var regexpLinkUser = /[\@]+([A-Za-z0-9-_]+)/gi;
-			var regexpLinkHash = / [\#]+([A-Za-z0-9-_]+)/gi;
-			
-			text = text.replace(regexpLinkUrl, "<a href=\"$1\">$1</a>");
-			text = text.replace(regexpLinkUser, "<a href=\"http://twitter.com/$1\">@$1</a>");
-			return text.replace(regexpLinkHash, ' <a href="http://search.twitter.com/search?q=&tag=$1&lang=all">#$1</a>');
-		}
-    };
-    
-    $.fn[pluginName] = function(options) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
-            }
-        });
-    }
+  $.fn[pluginName] = function(options) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+      }
+    });
+  }
 })(jQuery, window, document);
